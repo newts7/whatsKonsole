b23d6702a0e0df921c83ac747f9b3801b3a5111f
Divyanshu Srivastava <srivastavadi12@gmail.com>
2017-09-24 00:28:01 +0530

    group participants info added

---
+++ groupInfo.js
@@ -0,0 +1,15 @@
+var chats=Store.Chat.models;
+for (chat in chats){
+    var conversation=chats[chat];
+    if(conversation.__x_isGroup===true) {
+        var groupName = conversation.__x_formattedTitle;
+        var groupId = conversation.__x_id;
+        if (groupName.search("cclub AMA") != -1) {   /*If group is cclub then*/
+
+                var participants=conversation.__x_groupMetadata.participants.models;
+                console.log(participants);
+                console.log("\n");
+
+        }
+    }
+}
