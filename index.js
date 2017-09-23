6e282f028ffbd5e52b284f832712f88f80872ada
Divyanshu Srivastava <srivastavadi12@gmail.com>
2017-09-23 23:23:40 +0530

    message detection added

---
+++ index.js
@@ -0,0 +1,14 @@
+var chats=Store.Chat.models;
+for (chat in chats){
+    var conversation=chats[chat];
+    if(conversation.__x_isGroup===true) {
+        if (conversation.__x_unreadCount >= 1) {
+            var message = conversation.__x_previewMessage;
+            var sender = message.__x_author;
+            var text = message.__x_body;
+            //console.log(sender + " " + text);
+            /*Play with text now*/
+
+        }
+    }
+}
