f23a73f02c4feb023793f32158b0b22c77a7cc33
Divyanshu Srivastava <srivastavadi12@gmail.com>
2017-10-09 19:11:58 +0530

    modified selectedNumbers.js

---
+++ selectedNumbersDM.js
@@ -0,0 +1,54 @@
+
+var time=10000;
+
+var dotask=setInterval(sendMessage,time);
+var index=0;
+var contacts=["917275493769",
+             "919045321537"];
+
+function sendMessage()
+{
+    if(index==contacts.length) {
+        clearInterval(dotask);
+        return;
+    }
+    var Chats = Store.Chat.models;
+    var contact = contacts[index];
+    var message = "hello";
+    contact = contact + "@c.us";
+
+
+    flag = false;
+
+    for (chat in Chats) {
+        if (isNaN(chat)) {
+            continue;
+        }
+        ;
+        var temp = {};
+        temp.contact = Chats[chat].__x__formattedTitle;
+        temp.id = Chats[chat].__x_id;
+        if (temp.id.search(contact) != -1) {
+            flag = true;
+        }
+    }
+    console.log(contact);
+    if (!flag) {
+        Store.Chat.gadd(contact);
+    }
+
+    for (chat in Chats) {
+        if (isNaN(chat)) {
+            continue;
+        }
+        ;
+        var temp = {};
+        temp.contact = Chats[chat].__x__formattedTitle;
+        temp.id = Chats[chat].__x_id;
+        if (temp.id.search(contact) != -1) {
+            Chats[chat].sendMessage(message);
+
+        }
+    }
+    index++;
+}
